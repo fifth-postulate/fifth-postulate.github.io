8772815aa02f97ec0347a28ab1d68ca4dca3d278
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-10-29 14:21:48 +0100

    Create frontpage

---
+++ src/Main.elm
@@ -0,0 +1,64 @@
+module Main exposing (main)
+
+import Browser exposing (Document)
+import Html exposing (Html)
+import Html.Attributes as Attribute
+
+
+main: Program () Model msg
+main =
+    Browser.document
+        { init = \_ -> init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+init : (Model, Cmd msg)
+init =
+    ({}, Cmd.none)
+
+
+type alias Model =
+    {}
+
+
+view : Model -> Document msg
+view _ =
+    { title = "Fifth Postulate"
+    , body = [ header, footer ]
+    }
+
+
+header : Html msg
+header =
+    Html.section [ Attribute.class "header" ]
+        [ Html.h1 [] [ Html.text "Fifth Postulate" ]
+        , Html.ul []
+            [ Html.li [] [ Html.text "Consultancy" ]
+            , Html.li [] [ Html.text "Training" ]
+            , Html.li [] [ Html.text "Public Speaking" ]
+            ]
+        ]
+
+
+footer : Html msg
+footer =
+    Html.section [ Attribute.class "footer" ]
+        [ Html.dl []
+            [ Html.dt [] [ Html.text "KvK" ]
+            , Html.dd [] [ Html.text "65044649" ]
+            , Html.dt [] [ Html.text "Contact" ]
+            , Html.dd [] [ Html.text "info@fifth-postulate.nl" ]
+            ]
+        ]
+
+update : msg -> Model -> (Model, Cmd msg)
+update _ model =
+    (model, Cmd.none)
+
+
+subscriptions: Model -> Sub msg
+subscriptions _ =
+    Sub.none
